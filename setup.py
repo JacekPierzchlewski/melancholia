4c57400272382ebb15c8f7b665c3272cfd5dc28f
Jacek Pierzchlewski <jap@es.aau.dk>
2015-02-13 12:47:21 +0100

    All needed parameters in setup.py are in place

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@
 
 from distutils.core import setup
 setup(name='melancholia',
-      description='Python Distribution Utilities',
       version='1.0',
       py_modules=['melancholia'],
       author='Jacek Pierzchlewski',
       author_email='jap@es.aau.dk',
       url='https://www.irfducs.org/melancholia',
-      license='BSD 2-clause'
+      license=7'BSD 2-clause',
+      platforms='Linux, OS X',
+      description='Print Numpy arrays in a human readable way',
+      long_description = """Module prints Numpy arrays in a human readable way. Nice looking human readable way.
+      Arrays can be printed to a string variable or directly to a file."""
       )
