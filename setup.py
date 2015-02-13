a52bdc006ea3a600522f37dff7df6e5f26fd46bd
Jacek Pierzchlewski <jap@es.aau.dk>
2015-02-13 11:48:25 +0100

    setup.py is added

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""
+    This script installs 'melancholia'.
+"""
+
+from distutils.core import setup
+setup(name='melancholia',
+      description='Python Distribution Utilities',
+      version='1.0',
+      py_modules=['melancholia'],
+      author='Jacek Pierzchlewski',
+      author_email='jap@es.aau.dk',
+      url='https://www.irfducs.org/melancholia',
+      license='BSD 2-clause'
+      )
