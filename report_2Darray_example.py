8b894e1938733def327965752586d80722332002
Jacek Pierzchlewski <jap@es.aau.dk>
2015-02-13 10:23:48 +0100

    Version 1.0 of examples with 2D arrays is ready

--- report_2Darray_example.py
+++ report_2Darray_example.py
@@ -1,5 +1,6 @@
 """
-Example of using 'melancholia.py' module.
+Examples of using 'melancholia.py' modules with 2D Numpy arrays
+
 Run it:
 $ python report_2Darray_example.py.
 
@@ -16,7 +17,8 @@ Copyright (C) <2015>  Jacek Pierzchlewski
                                                       pierzchlewski dot jacek [at] gmail.com
 
 *Version*:
-    1.0-alpha  | 9-FEB-2015 : * Alpha version is ready. |br|
+    1.0-alpha  |  9-FEB-2015 : * Alpha version is ready. |br|
+    1.0        | 13-FEB-2015 : * Version 1.0 is ready. |br|
 
 *License*:
     BSD 2-Clause
@@ -27,9 +29,17 @@ import melancholia
 
 if __name__ == '__main__':
 
-    hReport = open('2Darrays.txt', 'w')      # Open the file
+
+    # %% 2 dimensional array printing directly to a file
+
+    # Array printing: The simplest usage of printing to a file
+    strInfo = "This array was printed directly to a file: \n\n"
+    mA = np.random.rand(10, 10)
+    melancholia.dumpA(mA, strFile='2Darrays.txt')
 
     # %% 2 dimensional array printing
+
+    hReport = open('2Darrays.txt', 'a')      # Open a file with matrices
 
     # The simplest usage
     strInfo = "The simplest example of 2D array printing: \n\n"
@@ -47,6 +57,12 @@ if __name__ == '__main__':
     strInfo = "2D array printing with a custom delimiter: \n\n"
     mA = np.random.rand(10, 10)
     strPrintedMat = strInfo + melancholia.printA(mA, strDelimiter='...')
+    hReport.write(strPrintedMat)
+
+    # No separation between the rows
+    strInfo = "No separation between the rows: \n\n"
+    mA = np.random.rand(10, 20)
+    strPrintedMat = strInfo + melancholia.printA(mA, iRowSpaces=0)
     hReport.write(strPrintedMat)
 
     # Custom format
@@ -71,14 +87,48 @@ if __name__ == '__main__':
     # ----------------------------------------------------------------------------------
     # 2 dimensional array printing with wrapped lines
 
+    # --------------------
+    # Lines wrapped because of the max allowed number of characters in a line:
+
     # The maximum number of characters in a single line
     strInfo = "Wrap line printing after 59 characters: \n\n"
     mA = np.random.rand(10, 10)
-    strPrintedMat = melancholia.printA(mA, iMaxCols=59)
+    strPrintedMat = strInfo + melancholia.printA(mA, iMaxCols=59)
     hReport.write(strPrintedMat)
 
     # The maximum number of characters in a single line, custom delimiter
     strInfo = "Wrap line printing after 120 characters, custom delimiter: \n\n"
     mA = np.random.rand(10, 20)
-    strPrintedMat = melancholia.printA(mA, iMaxCols=109, strDelimiter=' <---> ')
+    strPrintedMat = strInfo + melancholia.printA(mA, iMaxCols=109, strDelimiter=' <---> ')
     hReport.write(strPrintedMat)
+
+    # The maximum number of characters in a single line, no spearation between the lines
+    strInfo = "Wrap line printing after 120 characters, no separation between the lines: \n\n"
+    mA = np.random.rand(10, 20)
+    strPrintedMat = strInfo + melancholia.printA(mA, iMaxCols=109, iLineSpaces=0)
+    hReport.write(strPrintedMat)
+
+    # The maximum number of characters in a single line, no spearation between the lines, no separation between the rows
+    strInfo = "Wrap line printing after 120 characters, no separation between the lines, no separation between the rows: \n\n"
+    mA = np.random.rand(10, 20)
+    strPrintedMat = strInfo + melancholia.printA(mA, iMaxCols=109, iLineSpaces=0, iRowSpaces=0)
+    hReport.write(strPrintedMat)
+
+    # --------------------
+    # Lines wrapped because of the max allowed number of entries in a line:
+    strInfo = "Wrap line printing after 9 characters: \n\n"
+    mA = np.random.rand(10, 20)
+
+    # Add some nan, inf and -inf entries
+    mA[0, 0] = 1000.8
+    mA[3, 1] = np.nan
+    mA[9, 4] = np.nan
+    mA[1, 1] = np.inf
+    mA[6, 8] = np.inf
+    mA[5, 9] = np.inf
+    mA[5, 1] = -np.inf
+    mA[3, 5] = -np.inf
+    mA[4, 8] = -np.inf
+    strPrintedMat = strInfo + melancholia.printA(mA, iMaxEntr=9)
+    hReport.write(strPrintedMat)
+
