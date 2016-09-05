1e77f7825530961b63d0594ebf1513a005f63c3c
Jap <Pierzchlewski.Jacek@gmail.com>
2016-09-05 11:42:27 +0200

    Header updated and pep8 improvements in exaple #2 (2d array)

--- examples/example_2Darray.py
+++ examples/example_2Darray.py
@@ -7,19 +7,20 @@ $ python example_2Darray.py.
 The script will produce a file '2Darrays.txt'.
 
 
-Copyright (C) <2015>  Jacek Pierzchlewski
-                      jap [at] es dot aau dot dk
-                      pierzchlewski dot jacek [at] gmail.com
+
+Copyright (C) <2014-2016>  Jacek Pierzchlewski
+                           pierzchlewski dot jacek [at] gmail.com
 
 *Authors*:
-    Jacek Pierzchlewski, Aalborg University, Denmark. <jap@es.aau.dk>
-                                                      jap [at] es.aau.dk
-                                                      pierzchlewski dot jacek [at] gmail.com
+    Jacek Pierzchlewski, Aalborg University, Denmark.
+              pierzchlewski dot jacek [at] gmail.com
 
 *Version*:
     1.0-alpha  |  9-FEB-2015 : * Alpha version is ready. |br|
     1.0        | 13-FEB-2015 : * Version 1.0 is ready. |br|
     1.0-r1     | 18-FEB-2015 : * Comments cosmetics. |br|
+    1.0-r2     | 05-SEP-2016 : * pep8 improvements in code |br|
+
 
 *License*:
     BSD 2-Clause
@@ -29,7 +30,6 @@ import numpy as np
 import melancholia
 
 if __name__ == '__main__':
-
 
     # %% 2 dimensional array printed directly to a file
 
@@ -48,7 +48,8 @@ if __name__ == '__main__':
     hReport.write(strPrintedMat)
 
     # The simplest usage - vertical array Nx1
-    strInfo = "The simplest example of 2D array printing (vertical array): \n\n"
+    strInfo \
+        = "The simplest example of 2D array printing (vertical array): \n\n"
     mA = np.random.rand(10, 1)
     strPrintedMat = strInfo + melancholia.printA(mA)
     hReport.write(strPrintedMat)
@@ -72,19 +73,23 @@ if __name__ == '__main__':
     hReport.write(strPrintedMat)
 
     # Print array entries as integer with custom delimiter
-    strInfo = "2D array printing with integer format and custom delimiter: \n\n"
+    strInfo \
+        = "2D array printing with integer format and custom delimiter: \n\n"
     mA = np.random.randint(-100, 100, (40, 20))
-    strPrintedMat = strInfo + melancholia.printA(mA, strFormat='%d', strDelimiter=' <---> ')
+    strPrintedMat = strInfo + melancholia.printA(mA, strFormat='%d',
+                                                 strDelimiter=' <---> ')
     hReport.write(strPrintedMat)
 
     # Add array name and a header
-    strInfo = "2D array printing with integer format, custom delimiter and array header: \n\n"
+    strInfo = "2D array printing with integer format, "
+    strInfo += "custom delimiter and array header: \n\n"
     mA = np.random.rand(10, 10)
-    strPrintedMat = strInfo + melancholia.printA(mA, strFormat='%.2f', strDelimiter='...',
-                                                 strArrayName='mA', bPrintHeader=1)
+    strPrintedMat = \
+        strInfo + melancholia.printA(mA, strFormat='%.2f', strDelimiter='...',
+                                     strArrayName='mA', bPrintHeader=1)
     hReport.write(strPrintedMat)
 
-    # ----------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------
     # 2 dimensional array printing with wrapped lines
 
     # --------------------
@@ -99,19 +104,28 @@ if __name__ == '__main__':
     # The maximum number of characters in a single line, custom delimiter
     strInfo = "Wrap line printing after 120 characters, custom delimiter: \n\n"
     mA = np.random.rand(10, 20)
-    strPrintedMat = strInfo + melancholia.printA(mA, iMaxCols=109, strDelimiter=' <---> ')
+    strPrintedMat = strInfo + melancholia.printA(mA, iMaxCols=109,
+                                                 strDelimiter=' <---> ')
     hReport.write(strPrintedMat)
 
-    # The maximum number of characters in a single line, no spearation between the lines
-    strInfo = "Wrap line printing after 120 characters, no separation between the lines: \n\n"
+    # The maximum number of characters in a single line,
+    # no spearation between the lines
+    strInfo = "Wrap line printing after 120 characters, "
+    strInfo += "no separation between the lines: \n\n"
+
     mA = np.random.rand(10, 20)
-    strPrintedMat = strInfo + melancholia.printA(mA, iMaxCols=109, iLineSpaces=0)
+    strPrintedMat = strInfo + melancholia.printA(mA, iMaxCols=109,
+                                                 iLineSpaces=0)
     hReport.write(strPrintedMat)
 
-    # The maximum number of characters in a single line, no spearation between the lines, no separation between the rows
-    strInfo = "Wrap line printing after 120 characters, no separation between the lines, no separation between the rows: \n\n"
+    # The maximum number of characters in a single line,
+    # no spearation between the lines, no separation between the rows
+    strInfo = "Wrap line printing after 120 characters, no separation "
+    strInfo = "between the lines, no separation between the rows: \n\n"
+
     mA = np.random.rand(10, 20)
-    strPrintedMat = strInfo + melancholia.printA(mA, iMaxCols=109, iLineSpaces=0, iRowSpaces=0)
+    strPrintedMat = strInfo + melancholia.printA(mA, iMaxCols=109,
+                                                 iLineSpaces=0, iRowSpaces=0)
     hReport.write(strPrintedMat)
 
     # --------------------
@@ -131,4 +145,3 @@ if __name__ == '__main__':
     mA[4, 8] = -np.inf
     strPrintedMat = strInfo + melancholia.printA(mA, iMaxEntr=9)
     hReport.write(strPrintedMat)
-
