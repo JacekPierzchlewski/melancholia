b858fbfa7b1934cf6f123619c4d1afb702a7cf27
Jap <Pierzchlewski.Jacek@gmail.com>
2016-09-05 11:38:04 +0200

    Header updated and pep8 improvements in exaple #1 (1d array)

--- examples/example_1Darray.py
+++ examples/example_1Darray.py
@@ -7,19 +7,18 @@ $ python example_1Darray.py.
 The script will produce a file '1Darrays.txt'.
 
 
-Copyright (C) <2015>  Jacek Pierzchlewski
-                      jap [at] es dot aau dot dk
-                      pierzchlewski dot jacek [at] gmail.com
+Copyright (C) <2014-2016>  Jacek Pierzchlewski
+                           pierzchlewski dot jacek [at] gmail.com
 
 *Authors*:
-    Jacek Pierzchlewski, Aalborg University, Denmark. <jap@es.aau.dk>
-                                                      jap [at] es.aau.dk
-                                                      pierzchlewski dot jacek [at] gmail.com
+    Jacek Pierzchlewski, Aalborg University, Denmark.
+              pierzchlewski dot jacek [at] gmail.com
 
 *Version*:
     1.0-alpha  |  9-FEB-2015 : * Alpha version is ready. |br|
     1.0        | 13-FEB-2015 : * Version 1.0 is ready. |br|
     1.0-r1     | 18-FEB-2015 : * Comments cosmetics. |br|
+    1.0-r2     | 05-SEP-2016 : * pep8 improvements in code |br|
 
 *License*:
     BSD 2-Clause
@@ -29,7 +28,6 @@ import numpy as np
 import melancholia
 
 if __name__ == '__main__':
-
 
     # %% 1 dimensional array printed to a file
 
@@ -50,29 +48,34 @@ if __name__ == '__main__':
     # Array printing: integer elements
     strInfo = "1D array printing with integer format: \n\n"
     vA = np.random.randint(-1000, 1000, 100)
-    strPrintedVect =  strInfo + melancholia.printA(vA, strFormat='%d')
+    strPrintedVect = strInfo + melancholia.printA(vA, strFormat='%d')
     hReport.write(strPrintedVect)
 
     # Array printing: horizontal array printing
     strInfo = "1D array horizontal printing: \n\n"
     vA = np.random.rand(100)
-    strPrintedVect =  strInfo + melancholia.printA(vA, bVert1D=0)
+    strPrintedVect = strInfo + melancholia.printA(vA, bVert1D=0)
     hReport.write(strPrintedVect)
 
     # Array printing: horizontal array printing with integer elements
     strInfo = "1D array horizontal printing with integer format: \n\n"
     vA = np.random.randint(-100, 100, 20)
-    strPrintedVect =  strInfo + melancholia.printA(vA, bVert1D=0, strFormat='%d')
+    strPrintedVect = strInfo + melancholia.printA(vA, bVert1D=0,
+                                                  strFormat='%d')
     hReport.write(strPrintedVect)
 
-    # Array printing: horizontal array printing with integer elements and a header
-    strInfo = "1D array horizontal printing with integer format and a header: \n\n"
+    # Array printing: horizontal array printing with integer
+    # elements and a header
+    strInfo = "1D array horizontal printing "
+    strInfo += "with integer format and a header: \n\n"
+
     vA = np.random.randint(-100, 100, 20)
-    strPrintedVect =  strInfo + melancholia.printA(vA, bVert1D=0, strFormat='%d',
-                                                   strArrayName='vA', bPrintHeader=1)
+    strPrintedVect = \
+        strInfo + melancholia.printA(vA, bVert1D=0, strFormat='%d',
+                                     strArrayName='vA', bPrintHeader=1)
     hReport.write(strPrintedVect)
 
-    # ----------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------
     # 1 dimensional array printing with wrapped lines
 
     # --------------------
@@ -81,31 +84,47 @@ if __name__ == '__main__':
     # Array printing: horizontal array printing with shorter lines
     strInfo = "1D array horizontal printing with shorter lines: \n\n"
     vA = np.random.rand(100)
-    strPrintedVect =  strInfo + melancholia.printA(vA, bVert1D=0, iMaxCols=120)
+    strPrintedVect = strInfo + melancholia.printA(vA, bVert1D=0,
+                                                  iMaxCols=120)
     hReport.write(strPrintedVect)
 
-    # Array printing: horizontal array printing with shorter lines and fancy delimiter
-    strInfo = "1D array horizontal printing with shorter lines and custom delimiter: \n\n"
+    # Array printing: horizontal array printing with shorter lines and fancy
+    # delimiter
+    strInfo = "1D array horizontal printing "
+    strInfo += "with shorter lines and custom delimiter: \n\n"
+
     vA = np.random.randint(-100, 100, 1000)
-    strPrintedVect =  strInfo + melancholia.printA(vA, bVert1D=0, iMaxCols=120, strDelimiter='-|||-')
+    strPrintedVect = strInfo + melancholia.printA(vA, bVert1D=0,
+                                                  iMaxCols=120,
+                                                  strDelimiter='-|||-')
     hReport.write(strPrintedVect)
 
-    # Array printing: horizontal array printing with shorter lines fancy delimiter and a header
-    strInfo = "1D array horizontal printing with shorter lines, custom delimiter and a header: \n\n"
+    # Array printing: horizontal array printing with shorter lines fancy
+    # delimiter and a header
+    strInfo = "1D array horizontal printing with shorter lines,"
+    strInfo += "custom delimiter and a header: \n\n"
+
     vA = np.random.rand(100)
-    strPrintedVect =  strInfo + melancholia.printA(vA, bVert1D=0, iMaxCols=120, strDelimiter='-|||-',
-                                                   strArrayName='vA', bPrintHeader=1)
+    strPrintedVect = \
+        strInfo + melancholia.printA(vA, bVert1D=0, iMaxCols=120,
+                                     strDelimiter='-|||-', strArrayName='vA',
+                                     bPrintHeader=1)
     hReport.write(strPrintedVect)
 
-    # Array printing: horizontal array printing with shorter lines and no spaces between the lines
-    strInfo = "1D array horizontal printing with shorter lines and no spaces between the lines: \n\n"
+    # Array printing: horizontal array printing with shorter lines
+    # and no spaces between the lines
+    strInfo = "1D array horizontal printing with shorter lines "
+    strInfo += "and no spaces between the lines: \n\n"
     vA = np.random.rand(100)
-    strPrintedVect =  strInfo + melancholia.printA(vA, bVert1D=0, iMaxCols=120, iLineSpaces=0)
+    strPrintedVect \
+        = strInfo + melancholia.printA(vA, bVert1D=0, iMaxCols=120,
+                                       iLineSpaces=0)
     hReport.write(strPrintedVect)
 
     # --------------------
     # Lines wrapped because of the max allowed number of entries in a line:
-    strInfo = "1D array horizontal printing with shorter lines (7 elements in a line): \n\n"
+    strInfo = "1D array horizontal printing with shorter lines "
+    strInfo += "(7 elements in a line): \n\n"
     vA = np.random.rand(100)
-    strPrintedVect =  strInfo + melancholia.printA(vA, bVert1D=0, iMaxEntr=7)
+    strPrintedVect = strInfo + melancholia.printA(vA, bVert1D=0, iMaxEntr=7)
     hReport.write(strPrintedVect)
